c00116f7982fdd57820b71aa59e1954273250a16
mariaprang <marie.prangishvili@gmail.com>
2022-08-12 11:26:27 +0400

    removed a test plugin file

--- facebook_footer_link.php
+++ facebook_footer_link.php
@@ -1,1 +1,11 @@
 <?php
+/*
+ * Plugin Name: Facebook Footer Link
+ * Description: Adds social media link to the footer of the page
+ * Version: 1.0
+ */
+
+// Exit if accessed directly
+if(!defined('ABSPATH')){
+    exit();
+}
